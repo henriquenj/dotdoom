b31b180be0246fe9c7bd729c47918606eae213ea
Henrique Jung <henriquenj@gmail.com>
2026-02-19 21:56:00 +0100

    Adjust theme and window/magit defaults
    - switch Doom theme from doom-one to doom-material
    - add Spacemacs-style leader bindings for window actions:
      SPC w TAB (other-window) and SPC w m (maximize buffer)
    - show all commits in Magit log sections by default

--- config.el
+++ config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-material)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -42,6 +42,14 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
+
+(map! :leader
+      (:prefix "w"
+       :desc "Other window" "TAB" #'other-window
+       :desc "Maximize buffer" "m" #'doom/window-maximize-buffer))
+
+(with-eval-after-load 'magit
+  (setq magit-log-section-commit-count 0))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `with-eval-after-load' block, otherwise Doom's defaults may override your
