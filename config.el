8cd6dd96561e491c709fdae476ce3c35876c56f6
Henrique Jung <henriquenj@gmail.com>
2026-03-08 18:40:51 +0100

    Set diff-hl change marker to pipe in TTY

--- config.el
+++ config.el
@@ -38,6 +38,10 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+;; Neovim-style VCS markers in TTY line-number margin.
+(after! diff-hl-margin
+  (setf (alist-get 'change diff-hl-margin-symbols-alist) "|"))
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
