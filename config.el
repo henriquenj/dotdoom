5b8f50d54d97f1ca243ce63785712fce4b246886
Henrique Jung <henriquenj@gmail.com>
2026-03-08 18:55:02 +0100

    Hide diff-hl reference markers in TTY

--- config.el
+++ config.el
@@ -42,6 +42,11 @@
 (after! diff-hl-margin
   (setf (alist-get 'change diff-hl-margin-symbols-alist) "|"))
 
+;; Match GUI behavior in TTY: don't show staged hunks as reference markers.
+(after! diff-hl
+  (unless (display-graphic-p)
+    (setq diff-hl-highlight-reference-function nil)))
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
